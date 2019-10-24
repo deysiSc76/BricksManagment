1e852b534e66cfb70ffcb0f0441daf966db6b855
Enzo <enzomarecos@gmail.com>
2019-10-23 23:43:35 -0300

    data.sql para roles

--- src/main/java/com/fiuni/sd/bricks_management/domain/role/RoleDomain.java
+++ src/main/java/com/fiuni/sd/bricks_management/domain/role/RoleDomain.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
 import com.fiuni.sd.bricks_management.domain.base.BaseDomain;
 
 @Entity
+@Table(name = "roles")
 public class RoleDomain implements BaseDomain {
 
 	private static final long serialVersionUID = 1L;
