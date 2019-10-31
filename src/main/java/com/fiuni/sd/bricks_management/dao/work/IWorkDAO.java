e2e5d8a79189c7c80edf9a554821046a47d2c9b6
deysiSc76 <31452987+deysiSc76@users.noreply.github.com>
2019-10-31 11:48:20 -0400

    Update IWorkDAO.java
    work dao commit

--- src/main/java/com/fiuni/sd/bricks_management/dao/work/IWorkDAO.java
+++ src/main/java/com/fiuni/sd/bricks_management/dao/work/IWorkDAO.java
@@ -9,6 +9,6 @@ import com.fiuni.sd.bricks_management.domain.work.WorkDomain;
 
 @Repository
 public interface IWorkDAO extends CrudRepository<WorkDomain,Integer> {
-
+	public Page<WorkDomain> findByName(String name, Pageable pageable);
 	public Page<WorkDomain> findAll(Pageable pageable);
 }
